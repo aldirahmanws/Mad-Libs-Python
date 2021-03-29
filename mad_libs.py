7958281f9f036c9282fba3723df0e622f3630dc3
aldirahmanws <rahmanaldi290@gmail.com>
2021-03-29 11:50:27 +0700

    Initial commit

---
+++ mad_libs.py
@@ -0,0 +1,8 @@
+question = f"Question : Our school cafeteria has really .... food. Just thinking about makes stomach ..."
+print(question)
+
+adjective = input("Adjective = ")
+verb = input("Verb = ")
+
+mad_libs = f"Our school cafeteria has really {adjective} food. Just thinking about makes stomach {verb}."
+print(mad_libs)
